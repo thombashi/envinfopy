4f96c64aed7feab1b90ad0508369b7543f2e4873
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-06-02 17:03:49 +0900

    Modify keys of get_envinfo output

--- envinfopy/__init__.py
+++ envinfopy/__init__.py
@@ -10,12 +10,18 @@ import pkg_resources
 from .__version__ import __author__, __copyright__, __email__, __license__, __version__
 
 
+class Key:
+    UNAME = "uname"
+    PYTHON_IMPLEMENTATION = "python_implementation"
+    PYTHON_VERSION = "python_version"
+
+
 def get_envinfo(packages: Optional[Sequence[str]] = None) -> Dict[str, str]:
     uname = platform.uname()
     envinfo = {
-        "uname": "{} {} {} {}".format(uname.system, uname.node, uname.release, uname.machine),
-        "implementation": platform.python_implementation(),
-        "version": platform.python_version(),
+        Key.UNAME: "{} {} {} {}".format(uname.system, uname.node, uname.release, uname.machine),
+        Key.PYTHON_IMPLEMENTATION: platform.python_implementation(),
+        Key.PYTHON_VERSION: platform.python_version(),
     }
 
     if not packages:
@@ -25,23 +31,22 @@ def get_envinfo(packages: Optional[Sequence[str]] = None) -> Dict[str, str]:
         if not pkg:
             continue
 
-        key = "{} version".format(pkg)
         try:
-            envinfo[key] = pkg_resources.get_distribution(pkg).version
+            envinfo[pkg] = pkg_resources.get_distribution(pkg).version
         except pkg_resources.DistributionNotFound:
-            envinfo[key] = "not installed"
+            envinfo[pkg] = "not installed"
 
     return envinfo
 
 
 def dumps(packages: Optional[Sequence[str]] = None, format: Optional[str] = None) -> str:
     envinfo = get_envinfo(packages)
-    uname = envinfo.pop("uname")
-    implementation = envinfo.pop("implementation")
-    version = envinfo.pop("version")
+    uname = envinfo.pop(Key.UNAME)
+    implementation = envinfo.pop(Key.PYTHON_IMPLEMENTATION)
+    version = envinfo.pop(Key.PYTHON_VERSION)
 
     lines = ["uname: {}".format(uname), "{} version: {}".format(implementation, version)]
-    lines.extend(["{}: {}".format(key, value) for key, value in envinfo.items()])
+    lines.extend(["{} version: {}".format(key, value) for key, value in envinfo.items()])
 
     if format and format.strip().lower() == "markdown":
         lines = ["- {}".format(line) for line in lines]
--- tests/test_envinfo.py
+++ tests/test_envinfo.py
@@ -1,6 +1,7 @@
 import re
 
 import envinfopy
+from envinfopy import Key
 
 
 RE_VERSION = re.compile(r"[0-9]\.[0-9]+\.[0-9]+", re.MULTILINE)
@@ -12,17 +13,17 @@ class Test_get_envinfo:
         print(envinfo)
 
         assert len(envinfo["uname"]) >= 10
-        assert RE_VERSION.search(envinfo["version"])
-        assert RE_VERSION.search(envinfo["pytest version"])
-        assert RE_VERSION.search(envinfo["envinfopy version"])
+        assert RE_VERSION.search(envinfo[Key.PYTHON_VERSION])
+        assert RE_VERSION.search(envinfo["pytest"])
+        assert RE_VERSION.search(envinfo["envinfopy"])
 
     def test_abnormal_not_installed(self):
         envinfo = envinfopy.get_envinfo(["not-installed-pkg"])
         print(envinfo)
 
         assert len(envinfo["uname"]) >= 10
-        assert RE_VERSION.search(envinfo["version"])
-        assert envinfo["not-installed-pkg version"] == "not installed"
+        assert RE_VERSION.search(envinfo[Key.PYTHON_VERSION])
+        assert envinfo["not-installed-pkg"] == "not installed"
 
     def test_abnormal_empty(self):
         envinfo = envinfopy.get_envinfo([""])
