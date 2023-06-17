151f2a05d33cc26e6feadb289fc2e6dfb9626e13
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-06-18 00:50:48 +0900

    Add cli extras

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 markdown_requires = ["pytablewriter>=0.59.0,<2"]
 dev_requires = ["releasecmd>=0.9"]
 distro_requires = ["distro<2"]
-all_requires = markdown_requires + dev_requires + distro_requires
+cli_requires = markdown_requires + distro_requires
+all_requires = cli_requires + dev_requires
 
 setuptools.setup(
     name=MODULE_NAME,
@@ -61,6 +62,7 @@ setuptools.setup(
     install_requires=INSTALL_REQUIRES,
     extras_require={
         "all": all_requires,
+        "cli": cli_requires,
         "dev": dev_requires,
         "distro": distro_requires,
         "markdown": markdown_requires,
